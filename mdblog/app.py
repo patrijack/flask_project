f23bc98d18337946ab824bcf96d4bce18564b863
patrijack <patrixx@atlas.sk>
2020-09-01 15:53:59 +0000

    jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from .database import articles
+
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
@@ -13,9 +15,15 @@ def view_about():
 
 @flask_app.route("/articles/")
 def view_articles():
-	return render_template("articles.jinja")
+	return render_template("articles.jinja", articles=articles.items())
 
 @flask_app.route("/admin/")
 def view_admin():
 	return render_template("admin.jinja")
 
+@flask_app.route("/articles/<int:article_id>")
+def view_article(article_id):
+	article = articles.get(article_id)
+	if article:
+		return render_template("article.jinja", article=article)
+	return render_template("article_not_found.jinja", article_id=article_id)
---
+++ mdblog/database.py
@@ -0,0 +1,22 @@
+articles = {
+			1 : {
+				"title" : "Hello World",
+				"content" : "Labore labore ex sed sit ut consectetur dolor eu cupidatat consequat reprehenderit aliqua dolor proident tempor fugiat."
+				},
+			2 : {
+				"title" : "Hello World",
+				"content" : "Labore labore ex sed sit ut consectetur dolor eu cupidatat consequat reprehenderit aliqua dolor proident tempor fugiat."
+				},
+			3 : {
+				"title" : "Hello World",
+				"content" : "Labore labore ex sed sit ut consectetur dolor eu cupidatat consequat reprehenderit aliqua dolor proident tempor fugiat."
+				},
+			4 : {
+				"title" : "Hello World",
+				"content" : "Labore labore ex sed sit ut consectetur dolor eu cupidatat consequat reprehenderit aliqua dolor proident tempor fugiat."
+				},
+			5 : {
+				"title" : "Hello World",
+				"content" : "Labore labore ex sed sit ut consectetur dolor eu cupidatat consequat reprehenderit aliqua dolor proident tempor fugiat."
+				}
+			}
